db6b78d8fa38d4de6a6231dff28a12178ca95b65
Ryan Grove <ryan@wonko.com>
2008-11-10 22:40:24 -0800

    Test improvements, simple benchmarking.

--- test/test.rb
+++ test/test.rb
@@ -1,5 +1,28 @@
 require '../lib/jsmin'
 
-File.open('jslint.js', 'r') do |input|
-  File.open('out-ruby.js', 'w') {|output| output << JSMin.minify(input) }
+def benchmark_c(input, output)
+  start = Time.now
+  `jsmin < #{input} > #{output}`
+  finish = Time.now
+
+  puts "jsmin.c  [#{input}]: #{finish - start}"
 end
+
+def benchmark_ruby(input, output)
+  start = Time.now
+
+  File.open(input, 'r') do |i|
+    File.open(output, 'w') {|o| o << JSMin.minify(i) }
+  end
+
+  finish = Time.now
+
+  puts "jsmin.rb [#{input}]: #{finish - start}"
+end
+
+benchmark_c('js/jquery-1.2.6.js', 'out/c-jquery-1.2.6.js')
+benchmark_ruby('js/jquery-1.2.6.js', 'out/ruby-jquery-1.2.6.js')
+benchmark_c('js/jslint.js', 'out/c-jslint.js')
+benchmark_ruby('js/jslint.js', 'out/ruby-jslint.js')
+
+puts
